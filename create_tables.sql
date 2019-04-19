f8ece1646d76b67e366940904c8029d5946080cc
herb swanson <herb@debian-9.6>
2019-04-19 02:32:14 -0700

    result.php now working ... sql statements

--- create_tables.sql
+++ create_tables.sql
@@ -1,1 +1,4 @@
 CREATE TABLE `wp_bookshelf` (`googleid` char(64) not NULL , `title` varchar(200) NOT NULL, `subtitle` text, `author` char(64),  `publisher` char(64), `bookid` char(64) not NULL,`ind_type` char(64), `category` char(32),  `review` text,`knt` int default 0,`ip`  json default null, primary key (`googleid`));
+
+LOAD DATA INFILE "books_db.csv" INTO TABLE wp_bookshelf COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' ESCAPED BY '"' LINES TERMINATED BY '\n';
+
--- result.php
+++ result.php
@@ -7,14 +7,18 @@ header("Content-Type: application/json; charset=UTF-8");
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $response = [];
+    $host     = "localhost";
+    $user     = "herb";
+    $password = "redwine";
     @$transactionId = $request->transactionId;
+    @$personal_lib= $request->personal_lib;
+    @$personal_shelf= $request->personal_shelf;
+    $database = "bookshelf";
     switch ($transactionId) {
 
 	    case "viewLib":
 
 	            @$searchterm = $request->searchterm;
-	            @$personal_lib= $request->personal_lib;
-	            @$personal_shelf= $request->personal_shelf;
 	
 	            $query =  "SELECT knt,title,bookid,author,googleid 
 	                FROM wp_bookshelf  order by knt desc, title limit 50";
@@ -55,13 +59,71 @@ header("Content-Type: application/json; charset=UTF-8");
 		    $wpdb->close();
 	            break;
             case "saveBook":
+                @$googleid = $request->googleid;
+                $url = "https://www.googleapis.com/books/v1/volumes/" . $googleid;
+                $results = file_get_contents($url);
+                $request = json_decode($results);
+                $bookInfo = $request->volumeInfo;
+                    $id= $request->id;
+                    $title= str_replace("'","\'",$bookInfo->title);
+                    $subtitle= str_replace("'","\'", $bookInfo->subtitle);
+                    $author= str_replace("'","\'", $bookInfo->authors[0]);
+                    $publisher= str_replace("'","\'", $bookInfo->publisher);
+                    $ind_id= $bookInfo->industryIdentifiers[0]->identifier;
+                    $ind_type=  $bookInfo->industryIdentifiers[0]->type;
+                    $category=str_replace("'","\'", $bookInfo->categories[0]);
+                    $description= str_replace("'","\'", $bookInfo->description);
+                    $sql = "
+                        insert into wp_bookshelf
+                     (googleid,
+	            title,
+	            subtitle,
+	            author,
+	            publisher,
+	            bookid,
+	            ind_type,
+	            category,
+	            review
+                    ) VALUES (
+                    '$id',
+                    '$title',
+                    '$subtitle',
+                    '$author',
+                    '$publisher',
+                    '$ind_id',
+                    '$ind_type',
+                    '$category',
+                    '$description'
+                    )    
+                    on duplicate key update knt = knt + 1"; 
 
-                    $response['error']	= "got to save book block";
+		    $wpdb = new mysqli($host, $user, $password, $database);
+		    if ($wpdb->connect_errno) {
+	               $response['sqlerror'] = "$wpdb->connect_errno";
+	               break;
+		    }
+		    $response['query'] = $wpdb->query($sql);
+
+                    if ($personal_lib == '') {
+                        break;
+                    }
+                    $sql = "insert ignore into wp_bookshelf_personal
+                        (nick_id,
+                        bookid,
+                        shelf_name
+                        ) values (
+                        '$personal_lib',
+                        '$ind_id',
+                        '$personal_shelf')";
+
+		    $response['query'] = $wpdb->query($sql);
+
+                    $response['at-end']	= "got to save book block";
                     break;
                     
             default:
+                    $response['error'] = "invalid transactionId:$transactionId";
 
-            $response['error'] = "invalid transactionId:$transactionId";
-}
+            };
 	    echo json_encode($response);
 ?>
