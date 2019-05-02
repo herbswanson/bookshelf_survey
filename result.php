b4a31d6e14be0a984105b228d6d5de8169bdc965
Herb Swanson <herb@vps231314.vps.ovh.ca>
2019-05-02 09:21:32 -0400

    converted to using wordpress $wpdb pointer

--- result.php
+++ result.php
@@ -1,5 +1,18 @@
 <?php
+    function find_wordpress_base_path() {
+        $dir = dirname(__FILE__);
+        do {
+           if( file_exists($dir."/wp-config.php") ) {
+                return $dir;
+            }
+        } while( $dir = realpath("$dir/..") );
+        return null;
+    }
 
+    define( 'BASE_PATH', find_wordpress_base_path()."/" );
+    define('WP_USE_THEMES', false);
+    global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header, $wpdb;
+    require(BASE_PATH . 'wp-load.php');
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
    /*
@@ -8,14 +21,11 @@ header("Content-Type: application/json; charset=UTF-8");
     $postdata = file_get_contents("php://input");
     $request = json_decode($postdata);
     $response = [];
-    $host     = "localhost";
-    $user     = "herb";
-    $password = "redwine";
     @$transactionId = $request->transactionId;
     @$personal_lib= $request->personal_lib;
     @$personal_shelf= $request->personal_shelf;
 
-    $database = "bookshelf";
+    $query = '';
     switch ($transactionId) {
 
 	    case "viewLib":
@@ -48,7 +58,7 @@ header("Content-Type: application/json; charset=UTF-8");
 			
 			    if (  $personal_lib != "") { 
 		                $query = "select a.title,a.author,a.bookid,b.nick_id,b.shelf_name,a.knt,a.googleid
-		                          from wp_bookshelf a  inner join  wp_bookshelf_personal b on a.bookid=b.bookid
+		                          from wp_bookshelf a  inner join  wp_bookshelf_personal b on a.bookid=b.book_id
 		                          where b.nick_id = '$personal_lib' order by  title";
                                 break;
 	                    }
@@ -56,19 +66,10 @@ header("Content-Type: application/json; charset=UTF-8");
 		            $query =  "SELECT knt,title,bookid,author,googleid 
 		                FROM wp_bookshelf  order by knt desc, title limit 50";
                     }
-		
-		    $host     = "localhost";
-		    $user     = "herb";
-		    $password = "redwine";
-		    $database = "bookshelf";
-		    $wpdb = new mysqli($host, $user, $password, $database);
-		    if ($wpdb->connect_errno) {
-	               $response['sqlerror'] = "$wpdb->connect_errno";
-	               break;
-		    }
+
 		    $rows = array();
-		    $result = $wpdb->query($query);
-		
+		    $result = $wpdb->get_results($query);
+
 		    foreach ($result as $r ) {
 	               $rows[]=$r;
 		    }
@@ -81,24 +82,20 @@ header("Content-Type: application/json; charset=UTF-8");
                 $results = file_get_contents($url);
                 $request = json_decode($results);
                 $bookInfo = $request->volumeInfo;
-
-		    $wpdb = new mysqli($host, $user, $password, $database);
-		    if ($wpdb->connect_errno) {
-	               $response['sqlerror'] = "$wpdb->connect_errno";
-	               break;
-		    }
-                     $id= mysqli_real_escape_string($wpdb ,$request->id);
-                    $title= mysqli_real_escape_string($wpdb , $bookInfo->title);
-                    $subtitle= mysqli_real_escape_string($wpdb ,  $bookInfo->subtitle);
-                    $author= mysqli_real_escape_string($wpdb ,  $bookInfo->authors[0]);
-                    $publisher= mysqli_real_escape_string($wpdb ,  $bookInfo->publisher);
-                    $ind_id= mysqli_real_escape_string($wpdb , $bookInfo->industryIdentifiers[0]->identifier);
-                    $ind_type=  mysqli_real_escape_string($wpdb , $bookInfo->industryIdentifiers[0]->type);
-                    $category=mysqli_real_escape_string($wpdb ,  $bookInfo->categories[0]);
-                    $description= mysqli_real_escape_string($wpdb ,  $bookInfo->description);
-                    $sql = "
-                        insert into wp_bookshelf
-                     (googleid,
+                $id= $request->id;
+                $title= $bookInfo->title;
+                $subtitle= $bookInfo->subtitle;
+                $author=   $bookInfo->authors[0];
+                $publisher= $bookInfo->publisher;
+                $ind_id=  $bookInfo->industryIdentifiers[0]->identifier;
+                $ind_type=   $bookInfo->industryIdentifiers[0]->type;
+                $category=  $bookInfo->categories[0];
+                $description=$bookInfo->description;
+ 		$response['insert_book'] = $wpdb->query($wpdb->prepare(                
+            	"                                           
+	                insert into `wp_bookshelf`                                                           
+		    (
+	            googleid,
 	            title,
 	            subtitle,
 	            author,
@@ -107,34 +104,24 @@ header("Content-Type: application/json; charset=UTF-8");
 	            ind_type,
 	            category,
 	            review
-                    ) VALUES (
-                    '$id',
-                    '$title',
-                    '$subtitle',
-                    '$author',
-                    '$publisher',
-                    '$ind_id',
-                    '$ind_type',
-                    '$category',
-                    '$description')
-                    on duplicate key update knt = knt + 1"; 
-                    
-		    $response['query'] = $wpdb->query($sql);
+		    )
+	                values (%s,%s,%s,%s,%s,%s,%s,%s,%s)                                   
+	                on duplicate key update knt = knt + 1
+             "                                                                                                                                                                                                                                                                                                                                            
+               ,                                                                                                                                                                                                                                                                                                                                          
+                  $id,$title,$subtitle,$author,$publisher,$ind_id,$ind_type,$category,$description)) ; 
 
                     if ($personal_lib == '') {
                         break;
                     }
-                    $sql = "insert ignore into wp_bookshelf_personal
-                        (nick_id,
-                        bookid,
-                        shelf_name
-                        ) values (
-                        '$personal_lib',
-                        '$ind_id',
-                        '$personal_shelf')";
-
-		    $response['query'] = $wpdb->query($sql);
-
+		    $response['insert_shelf'] = $wpdb->query($wpdb->prepare(
+            	    "
+                    insert ignore into wp_bookshelf_personal 
+                (nick_id,book_id,shelf_name)
+                values (%s,%s,%s)
+            "
+             ,
+                     $personal_lib,$ind_id,$personal_shelf));
                     $response['at-end']	= "got to save book block";
                     break;
                     
